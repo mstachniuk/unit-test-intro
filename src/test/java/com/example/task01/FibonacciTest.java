62c33157d348154e6a5e9ff9de718e0641d2cf4e
Marcin Stachniuk <mstachniuk@gmail.com>
2017-07-10 19:55:59 +0200

    Tests for Fibonacci

---
+++ src/test/java/com/example/task01/FibonacciTest.java
@@ -0,0 +1,69 @@
+package com.example.task01;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FibonacciTest {
+
+    @Test
+    public void shouldReturnZeroForZero() {
+        // given
+        Fibonacci fibonacci = new Fibonacci();
+
+        // when
+        int result = fibonacci.fibonacci(0);
+
+        // then
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void shouldReturnOneForOne() {
+        // given
+        Fibonacci fibonacci = new Fibonacci();
+
+        // when
+        int result = fibonacci.fibonacci(1);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void shouldReturnOneForTwo() {
+        // given
+        Fibonacci fibonacci = new Fibonacci();
+
+        // when
+        int result = fibonacci.fibonacci(2);
+
+        // then
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void shouldReturnTwoForThree() {
+        // given
+        Fibonacci fibonacci = new Fibonacci();
+
+        // when
+        int result = fibonacci.fibonacci(3);
+
+        // then
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void shouldReturnEightForSix() {
+        // given
+        Fibonacci fibonacci = new Fibonacci();
+
+        // when
+        int result = fibonacci.fibonacci(6);
+
+        // then
+        assertEquals(8, result);
+    }
+}
